89e8a93471a6843d30c1041e72eefa8ce8b520a9
Moroshima <moroshimaitsuki@gmail.com>
2024-10-31 22:45:01 +0800

    immobilize navigation config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import navigation from "@data/navigation.json";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,6 +9,16 @@ export const metadata: Metadata = {
   title: "Moroshima's Blog",
   description: "却顾所来径，苍苍横翠微。",
 };
+
+const navigation = [
+  { url: "/", sc_name: "首页", en_name: "Home" },
+  { url: "/posts", sc_name: "文章", en_name: "Posts" },
+  { url: "/archives", sc_name: "归档", en_name: "Archives" },
+  { url: "/categories", sc_name: "分类", en_name: "Categories" },
+  { url: "/links", sc_name: "友链", en_name: "Links" },
+  { url: "/about", sc_name: "关于", en_name: "About" },
+  { url: "/rss.xml", sc_name: "订阅", en_name: "Feed" },
+];
 
 export default function RootLayout({
   children,
