744a7efba1a8c8e5723b1f81f69759eb14e35b4b
Moroshima <moroshimaitsuki@gmail.com>
2025-10-29 22:01:08 +0800

    feat: add build info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import globalConfig from "@data/configs/global.json";
 import navigationConfig from "@data/configs/navigation.json";
 import "./globals.css";
+import { execSync } from "child_process";
+import semver from "semver";
 
 const inter = Inter({ subsets: ["latin"] });
 const { title, description } = globalConfig;
@@ -15,6 +17,28 @@ export const metadata: Metadata = {
   title: title,
   description: description,
 };
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), "package.json"), "utf8"),
+);
+
+const buildInfo: { label: string; value: string }[] = [
+  {
+    label: "version",
+    value: `${packageJson.version}+${
+      semver.prerelease(packageJson.version) === null
+        ? null
+        : execSync("git rev-parse --short HEAD").toString()
+    }`,
+  },
+  { label: "next", value: packageJson.dependencies.next },
+  { label: "bundler", value: process.env.TURBOPACK ? "turbopack" : "webpack" },
+  { label: "react", value: packageJson.dependencies.react },
+  { label: "node", value: process.version },
+  { label: "arch", value: process.arch },
+  { label: "platform", value: process.platform },
+  { label: "buildTime", value: new Date().toISOString() },
+];
 
 export default function RootLayout({
   children,
@@ -36,11 +60,12 @@ export default function RootLayout({
         </div>
         {children}
         <div>
-          {
-            JSON.parse(
-              fs.readFileSync(path.join(process.cwd(), "package.json"), "utf8"),
-            ).version
-          }
+          <p>build info</p>
+          {buildInfo.map(({ label, value }, index) => (
+            <p key={`build-info-${index}`}>
+              {label}: {value}
+            </p>
+          ))}
         </div>
       </body>
     </html>
